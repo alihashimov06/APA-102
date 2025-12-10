64c64687b7b9c0a29584a9919eda70ac5c5ecd96
unknown <alihashimov2006az@gmail.com>
2025-12-11 00:53:06 +0400

    s

---
+++ 34-Front_To_BackSqlConnection/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _34_Front_To_BackSqlConnection.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
